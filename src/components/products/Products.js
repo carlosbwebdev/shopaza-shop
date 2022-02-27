4567f07c8ad0356ef8ddf86a41ac96ede2f850a1
Carlos Barros <carlosbwebdev@gmail.com>
2022-02-27 19:43:27 +0000

    first commit

---
+++ src/components/products/Products.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+import commerce from '../../lib/commerce';
+import ProductsList from './ProductsList';
+import '../../styles/App.css';
+
+const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    commerce.products
+      .list()
+      .then((products) => {
+        setProducts(products.data);
+      })
+      .catch((error) => {
+        console.log('There was an error fetching the products', error);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <div className="products-list">
+      <ProductsList products={products} />
+    </div>
+  );
+};
+
+export default Products;
