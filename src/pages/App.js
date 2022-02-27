68378f30a4bbc014d096d9835eb1e278aca3cbdd
Carlos Barros <carlosbwebdev@gmail.com>
2022-02-27 19:43:46 +0000

    first commit

---
+++ src/pages/App.js
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+import Products from '../components/products/Products';
+
+function App() {
+  return (
+    <>
+      <main className="app">
+        <h1>hello from store index page</h1>
+
+        <Products />
+      </main>
+    </>
+  );
+}
+
+export default App;
