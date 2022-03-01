30e1cebccd12d72ce4dfd11e9203a80354d7042e
Carlos Barros <carlosbwebdev@gmail.com>
2022-03-01 18:44:29 +0000

    first commit

---
+++ src/pages/productPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Product from '../components/products/product';
+
+const ProductPage = () => {
+  return (
+    <div>
+      <Product />
+    </div>
+  );
+};
+
+export default ProductPage;
