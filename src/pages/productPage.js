e521d48bd5c3d9db3a4d1e3e240b0ebd150a6e9e
Carlos Barros <carlosbwebdev@gmail.com>
2022-03-04 17:13:51 +0000

    first commit

--- src/pages/productPage.js
+++ src/pages/productPage.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import Product from '../components/products/product';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import styles from '../styles/productpage.module.css';
+import { stripHtml } from 'string-strip-html';
 
 const ProductPage = () => {
+  let { id } = useParams();
+  let navigate = useNavigate();
+  const [product, getProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch(`https://api.chec.io/v1/products/${id}`, {
+        headers: {
+          'X-Authorization': 'pk_34694a8f6cb2073db340e2319639ca0bee3c770a690d6',
+        },
+      });
+      const products = await res.json();
+      getProduct(products);
+    };
+    fetchProducts();
+  }, [id]);
+  // const meta = stripHtml(product.description);
+
+  // const { description } = stripHtml(product.description);
+  // console.log(description);
   return (
-    <div>
-      <Product />
-    </div>
+    <article key={product.id} className={styles.articleSection}>
+      <img
+        className={styles.productImage}
+        src={product.image?.url}
+        alt={product.name}
+      />
+      <h1>{product.name}</h1>
+      {/* <p>{meta}</p> */}
+      <br />
+      <button onClick={() => navigate('/')}>Go back</button>
+    </article>
   );
 };
 
