e95f126e882d7db0adf51b4ef6a8dd44614d79fb
Carlos Barros <carlosbwebdev@gmail.com>
2022-03-04 17:12:10 +0000

    small change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/products/:id" element={<ProductPage />} />
         </Routes>
 
         <footer>
