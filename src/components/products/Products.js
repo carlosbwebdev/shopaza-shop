01c60a811b480e2a9f498b7d43c5c74809abc73e
Carlos Barros <carlosbwebdev@gmail.com>
2022-03-04 17:12:49 +0000

    change to fetch instead of commerce lib

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -6,21 +6,19 @@ import '../../styles/App.css';
 const Products = () => {
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = () => {
-    commerce.products
-      .list()
-      .then((products) => {
-        setProducts(products.data);
-      })
-      .catch((error) => {
-        console.log('There was an error fetching the products', error);
-      });
-  };
-
   useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('https://api.chec.io/v1/products/', {
+        headers: {
+          'X-Authorization': 'pk_34694a8f6cb2073db340e2319639ca0bee3c770a690d6',
+        },
+      });
+      const products = await res.json();
+
+      setProducts(products.data);
+    };
     fetchProducts();
   }, []);
-
   return (
     <div className="products-list">
       <ProductsList products={products} />
@@ -29,3 +27,18 @@ const Products = () => {
 };
 
 export default Products;
+
+// const fetchProducts = () => {
+//   commerce.products
+//     .list()
+//     .then((products) => {
+//       setProducts(products.data);
+//     })
+//     .catch((error) => {
+//       console.log('There was an error fetching the products', error);
+//     });
+// };
+
+// useEffect(() => {
+//   fetchProducts();
+// }, []);
