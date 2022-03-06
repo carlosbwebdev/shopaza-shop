1421781580a869d73cb10e6674d98252887335f4
Carlos Barros <carlosbwebdev@gmail.com>
2022-03-06 19:22:06 +0000

    add about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import ProductPage from './pages/productPage';
 import Home from './pages/Home';
+import Aboutpage from './pages/aboutpage';
 
 function App() {
   return (
@@ -8,15 +9,17 @@ function App() {
       <div className="App">
         <nav>
           <Link to="/">Home</Link>
+          <Link to="/aboutpage">About</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products/:id" element={<ProductPage />} />
+          <Route path="/aboutpage" element={<Aboutpage />} />
         </Routes>
 
         <footer>
-          <h1>Made by Carlos Barros</h1>
+          <h1>I am the footer</h1>
         </footer>
       </div>
     </>
